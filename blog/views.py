4bd2cc9655ca901ffff652ec6b3521c1da5fb8e1
simplyObare <josephatowiny@gmail.com>
2025-05-24 02:41:10 +0300

    handling static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def blog_list(request):
+    return render(request, "blog/blog_list.html")
+
+
+def blog_detail(request):
+    return render(request, "blog/blog_detail.html")
